6c8a9853d0bcb93d5a5ebb6df5134292d229478b
Sean Davis <sdavis2702@gmail.com>
2021-07-17 18:20:03 -0500

    style changes for front page

--- front-page.php
+++ front-page.php
@@ -1,6 +1,18 @@
 <?php
 get_header();
+?>
 
-the_content();
+<section class="content-wrap">
 
+	<header class="content-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header>
+
+	<div class="content-body">
+		<?php the_content(); ?></php>
+	</div>
+
+</section>
+
+<?php
 get_footer();
