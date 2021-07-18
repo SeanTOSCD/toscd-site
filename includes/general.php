9ee6ea9f207e3f5773cf2b0a4f0b2c01b3cac845
Sean Davis <sdavis2702@gmail.com>
2021-07-18 10:35:03 -0500

    WIP add more styles

--- includes/general.php
+++ includes/general.php
@@ -2,3 +2,26 @@
 /**
  * General functions
  */
+
+/**
+ * Conditionally add body classes to the site structure
+ *
+ * @param $classes Already-applied body classes
+ *
+ * @return array
+ */
+function toscd_body_classes( $classes ) {
+	global $post;
+
+	if ( is_front_page() ) {
+		$classes[] = 'custom-landing';
+	}
+
+	// Add page slug to body class (ex: "page-about")
+	if ( isset( $post ) ) {
+		$classes[] =  $post->post_type . '-' . $post->post_name;
+	}
+
+	return $classes;
+}
+add_action( 'body_class', 'toscd_body_classes' );
